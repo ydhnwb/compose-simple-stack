78df27d11fb2ed728d8c0e548b4642a2fefba221
Prieyuda Akadita S <ydhnwb-pc@pop-os.localdomain>
2023-01-08 13:57:29 +0700

    Try using AppNemeTheme {...}

--- app/src/main/java/com/ydhnwb/learnjetpacknav/feature/login/LoginFragment.kt
+++ app/src/main/java/com/ydhnwb/learnjetpacknav/feature/login/LoginFragment.kt
@@ -1,8 +1,13 @@
 package com.ydhnwb.learnjetpacknav.feature.login
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rxjava2.subscribeAsState
+import androidx.compose.ui.Modifier
+import com.ydhnwb.learnjetpacknav.app.theme.LearnJetpackNavTheme
 import com.ydhnwb.learnjetpacknav.core.ComposeFragment
 import com.ydhnwb.learnjetpacknav.utils.set
 import com.zhuinden.simplestack.Backstack
@@ -17,14 +22,23 @@ class LoginFragment : ComposeFragment(){
 
         val isLoginEnabled = loginViewModel.isLoginEnabled.subscribeAsState(initial = false)
 
-        LoginScreenLayout(
-            username = username,
-            password = password,
-            usernameUpdater = loginViewModel.username::set,
-            passwordUpdater = loginViewModel.password::set,
-            onLoginClicked = loginViewModel::onLoginClicked,
-            onRegisterClicked = loginViewModel::onRegisterClicked,
-            isLoginEnabled = isLoginEnabled,
-        )
+        //LearnJetpackNavXXX is my app name
+        LearnJetpackNavTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                LoginScreenLayout(
+                    username = username,
+                    password = password,
+                    usernameUpdater = loginViewModel.username::set,
+                    passwordUpdater = loginViewModel.password::set,
+                    onLoginClicked = loginViewModel::onLoginClicked,
+                    onRegisterClicked = loginViewModel::onRegisterClicked,
+                    isLoginEnabled = isLoginEnabled,
+                )
+            }
+        }
+
     }
 }
